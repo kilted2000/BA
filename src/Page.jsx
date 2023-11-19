0066554931943ded5402b9a494d477e0aac69ff2
Richard <richardlwhittington@gmail.com>
2023-11-19 12:48:10 -0600

    fix imports

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,9 +1,9 @@
 "use client";
 //mport React from 'react';
-import Content from '../components/Content';
-import Pricing from '../components/pricing';
-import Cta from '../components/Cta';
-import Hero from '../components/Hero';
+import Content from './components/Content';
+import Pricing from './components/pricing';
+import Cta from './components/Cta';
+import Hero from './components/Hero';
 
 export default function Page() {
   return (

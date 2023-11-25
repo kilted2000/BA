0e6f789197f335fe644e1cb1e10ab709770c7972
Richard <richardlwhittington@gmail.com>
2023-11-24 21:28:41 -0600

    create new links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,28 +1,39 @@
-"use client"
-import  { useState } from 'react';
-import { Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem, NavLink } from 'reactstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPaw, faBone } from '@fortawesome/free-solid-svg-icons';
+"use client";
+import { useState } from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Collapse,
+  Nav,
+  NavItem,
+  NavLink,
+} from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaw, faBone } from "@fortawesome/free-solid-svg-icons";
 import { useAuth0 } from "@auth0/auth0-react";
 
-import PageLink from './PageLink';
+import PageLink from "./PageLink";
 //import AnchorLink from './AnchorLink';
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const { user, isLoading } = useAuth0();
+  const { user, isLoading, loginWithRedirect, logout } = useAuth0();
   const toggle = () => setIsOpen(!isOpen);
-  
-  
+
   return (
     <>
       <div className="header hislop">
         <div id="title">
-          <div className="container-fluid navcon" >
-             <Navbar light expand="lg" id="links" > 
-               <NavbarBrand id="heading" href="/">
-                <img src="/assets/namenoback.png" alt="BarkAmigos Title" className="cover" />
-              </NavbarBrand>  
+          <div className="container-fluid navcon">
+            <Navbar light expand="lg" id="links">
+              <NavbarBrand id="heading" href="/">
+                <img
+                  src="/assets/namenoback.png"
+                  alt="BarkAmigos Title"
+                  className="cover"
+                />
+              </NavbarBrand>
               <NavbarToggler onClick={toggle} />
               <Collapse isOpen={isOpen} navbar>
                 <Nav className="me-auto" navbar>
@@ -35,61 +46,85 @@ const NavBar = () => {
                   <NavItem>
                     <NavLink href="#cta">Sign Up</NavLink>
                   </NavItem>
-                   {!isLoading && !user && ( 
-                     <NavItem id="qsLoginBtn" className='log'>
-                      <a
+                  {!isLoading && !user && (
+                    <NavItem id="qsLoginBtn" className="log">
+                      {/* <a
                         href="/api/auth/login"
                         className="btn btn-primary btn-margin"
                         
                         >
                         Log in
-                      </a>
-                     </NavItem>
-                   )} 
-                   {user && (
+                      </a> */}
+                      <button className="btn btn-primary btn-margin" onClick={() => loginWithRedirect()}>
+                        Log In
+                      </button>
+                    </NavItem>
+                  )}
+                  {user && (
                     <>
-                    <Nav
-                      id="nav-mobile"
-                      className="justify-content-between "
-                      navbar
-                     >
-                      <NavItem>
-                        <PageLink href="../profile.jsx">
-                          My Profile
-                        </PageLink>
-                      </NavItem>
-                      <NavItem id="qsLogoutBtn">
-                        <PageLink
+                      <Nav
+                        id="nav-mobile"
+                        className="justify-content-between "
+                        navbar
+                      >
+                        <NavItem>
+                          <PageLink href="../profile.jsx">My Profile</PageLink>
+                        </NavItem>
+                        <NavItem id="qsLogoutBtn">
+                          {/* <PageLink
                           href="/api/auth/logout"
                           className="btn btn-link p-1"
                          >
                           Log out
-                        </PageLink>
-                      </NavItem>
-                    </Nav>
+                        </PageLink> */}
+                          <button className="btn btn-link p-1"
+                            onClick={() =>
+                              logout({
+                                logoutParams: {
+                                  returnTo: window.location.origin,
+                                },
+                              })
+                            }
+                          >
+                            Log Out
+                          </button>
+                        </NavItem>
+                      </Nav>
                     </>
-                  )} 
+                  )}
                 </Nav>
               </Collapse>
             </Navbar>
 
             <div className="row">
               <div className="col-md-6 col-sm-12">
-                <h1 className="big-heading mb-5">Dog Walking is Better with Two.</h1>
-                <button type="button" className="btn btn-dark btn-lg download-button">
-                  <FontAwesomeIcon icon={faPaw} />{' '}
-                  {!isLoading && !user && ( 
+                <h1 className="big-heading mb-5">
+                  Dog Walking is Better with Two.
+                </h1>
+                <button
+                  type="button"
+                  className="btn btn-dark btn-lg download-button"
+                >
+                  <FontAwesomeIcon icon={faPaw} />{" "}
+                  {!isLoading && !user && (
                     <a href="/api/auth/login" tabIndex={0}>
                       Sign In
                     </a>
-                   )} 
+                  )}
                 </button>
-                <button type="button" className="btn btn-dark btn-lg download-button">
+                <button
+                  type="button"
+                  className="btn btn-dark btn-lg download-button"
+                >
                   <FontAwesomeIcon icon={faBone} /> Sign Up
                 </button>
               </div>
               <div className="phone-img col-md-6 col-sm-12">
-                <img className="rotate_image img-fluid title-image" src="/assets/img-clear.jpeg" alt="logo-mockup" />
+                <img
+                  className="rotate_image img-fluid title-image"
+                  src="/assets/img-clear.jpeg"
+                  alt="logo-mockup"
+                />
               </div>
             </div>
           </div>
@@ -100,5 +135,3 @@ const NavBar = () => {
 };
 
 export default NavBar;
-
-

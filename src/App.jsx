03c41c77a53737e626bb903c0139f913ae7b7e72
Richard <richardlwhittington@gmail.com>
2023-12-01 22:51:56 -0600

    whhat

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+//import React from 'react'
 import {Cloudinary} from "@cloudinary/url-gen";
 import {AdvancedImage} from '@cloudinary/react';
 import {fill} from "@cloudinary/url-gen/actions/resize";
